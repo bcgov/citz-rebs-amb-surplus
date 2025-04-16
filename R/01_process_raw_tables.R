64741fd5e5c41d1d1d318d3c9194053deff87b7c
Rattray <David.Rattray@gov.bc.ca>
2025-04-16 16:22:14 -0700

    geocoding employee addresses
    working with gal data to start.

--- R/01_process_raw_tables.R
+++ R/01_process_raw_tables.R
@@ -185,3 +185,133 @@ write.csv(
   here("data/output/CleanedVFAAssetList.csv"),
   row.names = FALSE
 )
+
+
+## Employee Data ####
+Employee_GAL_BCGov = read_xlsx(here("data/2024-12-19 GAL Directory.xlsx")) |>
+  select(
+    Name,
+    DisplayName = `Display Name`,
+    FirstName = `First Name`,
+    LastName = `Last Name`,
+    City,
+    Street,
+    EmailAddress = `Email Address`,
+    EmployeeID = `Employee ID`,
+    JobTitle = `Job Title`,
+    Company,
+    Division,
+    Department
+  ) |>
+  mutate(InputStreet = Street, InputCity = City) |>
+  filter(InputStreet != "0 – Confidential Location") |> # Can't reveal their secrets
+  mutate(InputStreet = gsub("–", "-", InputStreet)) |> # weird character replacement
+  mutate(InputStreet = gsub(",", "", InputStreet)) |>
+  # test_semicolon <- Employee_GAL_BCGov |>
+  #   filter(grepl(";", InputStreet)) |>
+  #   mutate(InputStreet = gsub(";.*", "", InputStreet))
+  # mutate(InputStreet = gsub(";.*", "", InputStreet)) |>
+  mutate(InputStreet = gsub(";", " ", InputStreet)) |>
+  # test_flr <- Employee_GAL_BCGov |>
+  #   filter(grepl("[Ff][l][r] [0-9]+ - ", InputStreet))
+  mutate(InputStreet = gsub("[Ff][l][r] +[0-9]+ - ", "", InputStreet)) |>
+  # test_floor <- Employee_GAL_BCGov |>
+  #   filter(grepl("[0-9]+[snrt][tdh] [Ff][l][o][o][r] - ", InputStreet))
+  mutate(
+    InputStreet = gsub(
+      "[0-9]+[snrt][tdh] [Ff][l][o][o][r][ ]?[-]?[ ]?",
+      "",
+      InputStreet
+    )
+  ) |>
+  mutate(
+    InputStreet = gsub("[0-9]+[snrt][tdh] [Ff][l][r]", "", InputStreet)
+  ) |>
+  mutate(
+    InputStreet = gsub(
+      "[:]?[-]?[ ]?[P][.]?[O][.]?[ ][B][Oo][Xx].*",
+      "",
+      InputStreet
+    )
+  ) |>
+  mutate(InputStreet = gsub("[Ff][l][r][ ]+[0-9][NESW]", "", InputStreet)) |>
+  mutate(InputStreet = gsub("#", "", InputStreet)) |>
+  mutate(InputStreet = gsub("[.]", "", InputStreet)) |>
+  mutate(
+    InputStreet = gsub("Mailing Address[:]?", "", InputStreet)
+  ) |>
+  # Box 820 2250 West Trans Canada Highway
+  # 150 - 4600 Jacombs Road Richmond BC Canada V6V 3B1
+  mutate(
+    InputStreet = na_if(InputStreet, ""),
+    InputStreet = na_if(InputStreet, " "),
+    InputCity = na_if(InputCity, ""),
+    InputCity = na_if(InputCity, " ")
+  )
+
+EmployeeAddressList <- Employee_GAL_BCGov |>
+  select(Street, City, InputStreet, InputCity) |>
+  filter(!is.na(InputStreet)) |>
+  filter(!is.na(InputCity)) |>
+  distinct() |>
+  mutate(GeoStreet = "", GeoType = "", Score = "", Precision = "")
+# test <- EmployeeAddressList |>
+#   filter(is.na(InputStreet))
+# test <- EmployeeAddressList |>
+#   filter(grepl("P.O.", InputStreet))
+# Use geocoder to improve addresses
+API_KEY = read.csv("C:/Projects/credentials/bc_geocoder_api_key.csv") |> pull()
+query_url = 'https://geocoder.api.gov.bc.ca/addresses.geojson?addressString='
+
+for (ii in 1:nrow(EmployeeAddressList)) {
+  location <- paste0(
+    str_replace_all(EmployeeAddressList[ii, "InputStreet"], " ", "%20"),
+    "%20",
+    str_replace_all(EmployeeAddressList[ii, "InputCity"], " ", "%20")
+  )
+  req <- request(paste0(query_url, location)) |>
+    req_headers(API_KEY = API_KEY) |>
+    req_perform()
+  resp <- req |> resp_body_json()
+  EmployeeAddressList$GeoStreet[ii] <- resp$features[[1]]$properties$fullAddress
+  EmployeeAddressList$GeoType[ii] <- resp$features[[
+    1
+  ]]$properties$matchPrecision
+  EmployeeAddressList$Precision[ii] <- resp$features[[
+    1
+  ]]$properties$precisionPoints
+  EmployeeAddressList$Score[ii] <- resp$features[[1]]$properties$score
+}
+
+EmployeeAddressList <- EmployeeAddressList |>
+  filter(Score >= 90 | Precision == 100 & Score > 80)
+
+write.csv(
+  EmployeeAddressList,
+  here("PBI/Data/GalEmployeeAddress.csv"),
+  row.names = FALSE
+)
+
+Employee_EstablishmentReportBCGov = read_xlsx(
+  here("data/Establishment Report - All BCPS - 2025-02-24.xlsx"),
+  sheet = "Establishment 2025-02-24"
+) |>
+  filter(`Empl Status` != "NULL") |>
+  select(
+    EmployeeID = `Empl ID`,
+    Company = Organization,
+    Division = `Program Division`,
+    Department = `Program Branch`,
+    DepartmentID = `DeptID`
+  )
+
+Employee_HeadcountCITZ = read_xlsx(
+  here("data/Headcount by Classification - All BCPS - 2025-02-24.xlsx"),
+  sheet = "Headcount 2025-02-24"
+)
+Employee_TeleworkBCGovPSA = read_xlsx(
+  here(
+    "data/Staff Data For Telework Data Collection - All BCPS - 2025-03-06.xlsx"
+  ),
+  sheet = "Telework All Orgs 2025-03-06"
+)
