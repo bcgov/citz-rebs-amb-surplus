b6cbcdd2cee6fb456302b35a8553d409f406efcf
Rattray <David.Rattray@gov.bc.ca>
2025-06-12 09:19:22 -0700

    finish connectivity data aggregation

--- R/03_process_network_data.R
+++ R/03_process_network_data.R
@@ -8,15 +8,46 @@ library(tidyr)
 library(tools)
 library(arrow)
 
-connectivity_raw <- read_parquet(here("data/Network_Person_Counts.parquet"))
+connectivity_raw <- read_parquet(here("data/Network_Person_Counts.parquet")) |>
+  select(AddressLink, street_address, approx_users, date, dotw, weekday_yn) |>
+  filter(date >= "2025-01-01") |>
+  filter(weekday_yn != "Weekend") |>
+  group_by(AddressLink, street_address, dotw) |>
+  summarize(
+    average_daily_users = mean(approx_users)
+  ) |>
+  pivot_wider(names_from = dotw, values_from = average_daily_users) |>
+  relocate(
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    .after = street_address
+  ) |>
+  rowwise() |>
+  mutate(WeeklyAverage = mean(c_across(c(Monday:Friday)), na.rm = TRUE))
 
+write.csv(
+  connectivity_raw,
+  here("PBI/data/R_Connectivity_Table.csv"),
+  row.names = FALSE
+)
+
+
+# list <- connectivity_raw |> select(AddressLink) |> arrange() |> distinct()
+
+# Attempt Geocoding ####
 addresses <- connectivity_raw |>
   select(street_address) |>
   distinct() |>
   # mutate(split_city = str_extract(street_address, "([^,]*$)")) |>
   mutate(clean_address = gsub(",", " ", street_address)) |>
-  mutate(geocoder_address = "", geocoder_score = "", geocoder_precision = "")
+  mutate(geocoder_address = "", geocoder_score = "", geocoder_precision = "") |>
+  # add address cleanup steps
+  mutate(test = gsub("#", "", street_address))
 
+write_xlsx(addresses, here("data/address_first_pass.xlsx"))
 
 # Use geocoder to improve addresses
 API_KEY = read.csv("C:/Projects/credentials/bc_geocoder_api_key.csv") |> pull()
@@ -39,23 +70,23 @@ for (ii in 1:nrow(addresses)) {
   addresses$geocoder_score[ii] <- resp$features[[1]]$properties$score
 }
 
-AddressListFinal <- AddressList |>
+AddressTable <- addresses |>
   separate_wider_delim(
-    geo_name,
+    geocoder_address,
     delim = ",",
-    names = c("geo_Street", "geo_City", "Province"),
+    names = c("Address", "City", "Province"),
     too_few = "align_start"
   ) |>
   mutate(
-    geo_Street = trimws(geo_Street),
-    geo_City = trimws(geo_City),
+    Address = trimws(Address),
+    City = trimws(City),
     Province = trimws(Province),
-    score = as.numeric(score),
-    precision = as.numeric(precision)
-  ) |>
-  mutate(
-    geo_Street = gsub("--", "-", geo_Street),
-  ) |>
+    geocoder_score = as.numeric(geocoder_score),
+    geocoder_precision = as.numeric(geocoder_precision)
+  ) #|>
+mutate(
+  geo_Street = gsub("--", "-", geo_Street),
+) |>
   mutate(
     Address = case_when(
       score >= 85 & precision >= 99 ~ geo_Street,
